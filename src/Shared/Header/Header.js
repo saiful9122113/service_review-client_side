aa4f709ee91fc680399f940de300f88bc1f86dad
Md. Saiful Islam <saiful9122113@gmail.com>
2022-11-08 02:19:57 +0600

    most of  nav part added here

--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -1,11 +1,52 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import image from "../../images/icon.png"
 
 const Header = () => {
-    return (
-        <div>
-            <h3>This is header</h3>
+
+    const menuItems = <>
+        <li className="font-simebold"><Link to='/'>Home</Link></li>
+    </>
+
+  return (
+    <div className="navbar h-24 mb-12 bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            {menuItems}
+          </ul>
         </div>
-    );
+        <Link to="/" className="btn btn-ghost normal-case text-xl"><img className="w-2/5" src={image} alt="" /></Link>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal p-0">
+         {menuItems}
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <a className="btn">Get started</a>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
