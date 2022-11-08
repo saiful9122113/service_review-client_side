0c2b834a2159e51376d4763c24e9425f4206fc4b
Md. Saiful Islam <saiful9122113@gmail.com>
2022-11-08 15:37:49 +0600

    services component added here

--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import ServiceCard from "../../Shared/SerciceCard/ServiceCard";
 
 const Services = () => {
-    return (
-        <div>
-            <h3>Services</h3>
-        </div>
-    );
+  const [services, setServices] = useState([]);
+
+  useEffect(() => {
+    fetch('services.json')
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  }, []);
+
+  return (
+    <div>
+      <div className="text-center">
+        <p className="text-2xl font-bold text-orange-600">Service</p>
+        <h2 className="text-4xl font-semibold">Our Services Area</h2>
+      </div>
+      <div className="grid gap-6 grid-cols-1 md:grid-cols:2 lg:grid-cols-3">
+        {
+          services.map(service=><ServiceCard
+          key={service._id}
+          service={service}
+          ></ServiceCard>)
+        }
+      </div>
+    </div>
+  );
 };
 
 export default Services;
